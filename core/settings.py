3b6d25792d138db945fd4d3b6b4ae5ed4bcdf4f1
Julian Stone <julianstone1969@gmail.com>
2026-04-05 18:27:47 +0100

    update settings

--- core/settings.py
+++ core/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 # The absolute path to the directory where collectstatic will gather static files for deployment.
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
