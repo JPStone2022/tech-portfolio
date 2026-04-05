617033af60be64cba32a2c91f085ea6d338c3e60
Julian Stone <julianstone1969@gmail.com>
2026-04-05 20:46:12 +0100

    add a test script to test celery functionality

--- ai_concepts/tasks.py
+++ ai_concepts/tasks.py
@@ -2,14 +2,14 @@ import time
 from celery import shared_task
 from portfolio.services.orchestrate_ai_learning_generation_regional import run_auto_generator
 
-# @shared_task
-# def generate_ai_course_background(limit=1, weeks=4, locale='uk'):
-#     """
-#     This function will be picked up by the Celery worker and executed 
-#     in the background, freeing up the main Django server thread.
-#     """
-#     run_auto_generator(limit=limit, weeks=weeks, locale=locale)
-#     return f"Successfully generated {limit} courses in the background!"
+@shared_task
+def generate_ai_course_background(limit=1, weeks=4, locale='uk'):
+    """
+    This function will be picked up by the Celery worker and executed 
+    in the background, freeing up the main Django server thread.
+    """
+    run_auto_generator(limit=limit, weeks=weeks, locale=locale)
+    return f"Successfully generated {limit} courses in the background!"
 
 # --- NEW DUMMY TASK ---
 @shared_task
